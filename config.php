a21930a35cfeb28c4193b213e2c260cf6aeffc77
Arunodhaya <arunarajagopal98@gmail.com>
2019-05-05 16:57:34 +0430

    SocialLogin

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    // your app consumer key
+    define( 'CONSUMER_KEY', 'ZFd8OJPABFEg9fDw6FPbvtLXY'); 
+
+    // your app consumer secret
+    define( 'CONSUMER_SECRET', 'lpSerlfC4gP7ZSleOcZSU5SL7bs2gdkfpGZv1fTcNO7zMdzysk');
+
+    // your app callback url
+    define( 'OAUTH_CALLBACK', 'http://localhost/Saran/login_fb_twitter/index.php' );
